e0133f5c61605cf628d1a3316cf90f0896d3b9f4
adamgoyer <adam@adamgoyer.com>
2022-01-23 20:16:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Hello, Practice Spellbook!')
+#import this
+#import math
+import pandas as pd
+import random
+# import matplotlib.pyplot as plt
+
+print('I cast 100 Magic Missiles into the Darkness!')
+"""Roll a D20 100 times"""
+for i in range(3):
+  print(random.randint(1,20),end=' ')
+
+
