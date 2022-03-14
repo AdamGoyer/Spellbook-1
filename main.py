a1fb7c349de45d932ad6c39ff091e44f5f673fa4
Adam Goyer <adam@adamgoyer.com>
2022-03-14 05:17:17 +0000

    added matplotlib and seaborn

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 print('Hello, Practice Spellbook!')
 #import this
 #import math
+import numpy as np
 import pandas as pd
-import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+matplotlib inline
+#pd.__version__ # Version Check doesn't seem to be working in replet...
+#import random
 # import matplotlib.pyplot as plt
 
-print('I cast 100 Magic Missiles into the Darkness!')
-"""Roll a D20 100 times"""
-for i in range(3):
-  print(random.randint(1,20),end=' ')
+#print('I cast 100 Magic Missiles into the Darkness!')
+#"""Roll a D20 100 times"""
+#for i in range(3):
+#  print(random.randint(1,20),end=' ')
+
+print('---')
+
+def test(x,y,a,b): 
+  if x == y and a == b:
+    print('True is aaaa')
+  else:
+    print('False is xyab')
+  
+x = 'a'
+y = 'a'
+a = 'a'
+b = 'a'
+
+(test(x,y,a,b))
 
 
+
+def number_tracks(day,city):
+    track_list = df[df['day'] == day]
+    track_list = track_list[track_list['city'] == city]
+    return(track_list)
+    track_list_count = track_list['user_id'].count()
+    return(track_list_count)
+    print(track_list_count)
